3705b419c9de1a94934aa57282444184f5ab9def
segusa <sebastian.gutierrez@sansano.usm.cl>
2020-10-17 21:39:03 -0300

    clienteReady

--- client/clientes.go
+++ client/clientes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "context"
 	"encoding/csv"
 	"fmt"
 	"io"
@@ -8,7 +9,13 @@ import (
 	"os"
 	"sync"
 	"time"
+	"strconv"
+	"google.golang.org/grpc"
 	pb "../proto"
+)
+
+const (
+	address  = "localhost:50051"
 )
 
 var wg = &sync.WaitGroup{}
@@ -41,11 +48,50 @@ func ordenesDePymes(period time.Duration){
 
 func clienteDePyme(ordenp []string,period time.Duration){
 	defer wg.Done()
-	fmt.Printf("id: %s Producto %s Valor %s Tienda %s Destino %s Prioritario %s\n ", ordenp[0], ordenp[1], ordenp[2], 
-			ordenp[3], ordenp[4], ordenp[5])
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewOrdenServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	tip := "normal" 
+
+	if ordenp[5] == "1" {
+		tip = "prioritario"
+	}
+
+	val,err := strconv.ParseInt(ordenp[2], 10, 64)
+
+	if err != nil {
+		fmt.Printf("no")
+	}
+
+	r, err := c.ReplyToOrder(ctx, &pb.SendToOrden{IdPaquete : ordenp[0],Tipo : tip ,Nombre :  ordenp[1],
+		Valor : val,Origen : ordenp[3],Destino : ordenp[4]})
+
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+
+	seg := r.GetSeguimiento() 
+
+
+	//fmt.Printf("id: %s Producto %s Valor %s Tienda %s Destino %s Prioritario %s\n ", ordenp[0], ordenp[1], ordenp[2], 
+	//		ordenp[3], ordenp[4], ordenp[5])
 	for {
 		time.Sleep(period * time.Second)
-		if true {
+
+        r, err := c.GetState(ctx, &pb.ReplyFromOrden{Seguimiento : seg})
+        if err != nil {
+		    log.Fatalf("could not greet: %v", err)
+	    }
+
+	    est := r.GetEstado()
+		if est == "Recibido" || est == "No Recibido" {
 			break
 		}
 	}
@@ -80,11 +126,43 @@ func ordenesDeRetail(period time.Duration){
 
 func clienteDeRetail(ordenr []string,period time.Duration){
 	defer wg.Done()
-	fmt.Printf("id: %s Producto %s Valor %s Tienda %s Destino %s\n ", ordenr[0], ordenr[1], ordenr[2], 
-			ordenr[3], ordenr[4])
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewOrdenServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	val,err := strconv.ParseInt(ordenr[2], 10, 64)
+
+	if err != nil {
+		fmt.Printf("no")
+	}
+
+	r, err := c.ReplyToOrder(ctx, &pb.SendToOrden{IdPaquete : ordenr[0],Tipo : "retail",Nombre :  ordenr[1],
+		Valor : val,Origen : ordenr[3],Destino : ordenr[4]})
+
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+
+	seg := r.GetSeguimiento() 
+
+	//fmt.Printf("id: %s Producto %s Valor %s Tienda %s Destino %s\n ", ordenr[0], ordenr[1], ordenr[2], 
+	//		ordenr[3], ordenr[4])
 	for {
 		time.Sleep(period * time.Second)
-		if true {
+
+        r, err := c.GetState(ctx, &pb.ReplyFromOrden{Seguimiento : seg})
+        if err != nil {
+		    log.Fatalf("could not greet: %v", err)
+	    }
+
+	    est := r.GetEstado()
+		if est == "Recibido" || est == "No Recibido" {
 			break
 		}
 	}
